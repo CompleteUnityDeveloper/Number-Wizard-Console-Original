9f40430e6bda5f43aff2881243566936e9220470
github@bentristem.com <github@bentristem.com>
2014-09-23 13:13:07 +0100

    B04 - Responding To Key Presses

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -13,10 +13,19 @@ public class NumberWizards : MonoBehaviour {
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick it " + min);
+		
+		print ("Is the number higher or lower than 500?");
+		print ("Up = higher, down = lower, return = equal");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print("Up arrow pressed");
+		}
+		
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print("Down arrow pressed");
+		}
 	}
 }
