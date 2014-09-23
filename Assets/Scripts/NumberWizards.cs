55eacabe1e7cda294c6e557aef62316baeaadac3
github@bentristem.com <github@bentristem.com>
2014-09-23 14:19:43 +0100

    B06 - Scope and Context of Variables

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -3,26 +3,33 @@ using System.Collections;
 
 public class NumberWizards : MonoBehaviour {
 	// Use this for initialization
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+	
 	void Start () {
+		max = max + 1;
+		
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
-		
-		int max = 1000;
-		int min = 1;
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick it " + min);
 		
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess);
 		print ("Up = higher, down = lower, return = equal");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("Up arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess);
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("Down arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess);
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print("I won!");
 		}
