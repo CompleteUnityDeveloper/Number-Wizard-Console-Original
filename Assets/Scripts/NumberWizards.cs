9ab5f52859c754468424e01804b23f5963527774
github@bentristem.com <github@bentristem.com>
2014-09-23 12:46:04 +0100

    B03 - Variables For Things That Change

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -7,6 +7,12 @@ public class NumberWizards : MonoBehaviour {
 	void Start () {
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
+		
+		int max = 1000;
+		int min = 1;
+		
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick it " + min);
 	}
 	
 	// Update is called once per frame
