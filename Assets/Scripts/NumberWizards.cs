9a7cd1e6346b2d4e986b167d316d7b70fc52c7cd
github@bentristem.com <github@bentristem.com>
2014-09-23 12:32:45 +0100

    B02 - Printing Text to the Console

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
