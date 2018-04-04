7c1c768306d56b79e75f8f377ee720f9d015399e
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-04 15:33:50 +0100

    Getting And Responding to User Input
    Introducing IF Statement

--- Number Wizard Remastered/Assets/NumberWizard.cs
+++ Number Wizard Remastered/Assets/NumberWizard.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NumberWizard : MonoBehaviour {
+public class NumberWizard : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         print("Welcome to Number Wizard");
         print("Pick a number, but don't tell me!");
@@ -18,11 +19,29 @@ public class NumberWizard : MonoBehaviour {
         print("The highest number you can pick is " + max); //example statment
         print("The lowest number you can pick it " + min);  //challenge statement
 
+        print("Is the number higher or lower than 500?"); // Ask question
+        print("Up = higher, down = lower, return = equal"); // Tell user what to do
+
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        //Example: Check what the user has pressed... if up, print to console "Up"
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up arrow pressed");
+        }
+
+        //Challenge: Check what the user has pressed... down and correct+ printing to console
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down arrow pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won!");
+        }
+    }
 }
