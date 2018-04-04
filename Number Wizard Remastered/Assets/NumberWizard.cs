5a5226cea73ec4733b6cd960be18e44509b6251f
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-04 18:14:12 +0100

    Your First Method and Refactor Challenge

--- Number Wizard Remastered/Assets/NumberWizard.cs
+++ Number Wizard Remastered/Assets/NumberWizard.cs
@@ -4,60 +4,60 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    // Moving these variables out of the Start Method, means we can use them again in the update and other methods too.
-    // We have broadened their scope.
-    int min = 1; 
-    int max = 1000; 
-    int guess = 500;
-    //int max 1001; explain why this is not the solution
+    int min; 
+    int max; 
+    int guess;
 
     // Use this for initialization
     void Start()
     {
+        StartGame();
+    }
+
+    // Simplifying game start Instruction
+    void StartGame()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+
         print("Welcome to Number Wizard");
         print("Pick a number, but don't tell me!");
+        
+        print("The highest number you can pick is " + max);
+        print("The lowest number you can pick it " + min);
 
-        // 
-
-        //We combine strings with our new variable, note it is combining a string and interger together for us.
-        print("The highest number you can pick is " + max); //example statment
-        print("The lowest number you can pick it " + min);  //challenge statement
-
-        print("Is the number higher or lower than " + guess + "?"); // Ask question
-        print("Up = higher, down = lower, return = equal"); // Tell user what to do
-
+        print("Is the number higher or lower than " + guess + "?");
+        print("Up = higher, down = lower, return = equal");
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        max = 1001; //challenge bug fix
-        //max = max + 1; Explain why this isn't a solution
-        //Example: Check what the user has pressed... if up, print to console "Up"
+        max = 1001;
+        //Challenge Refactor
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-             
-            //print("Up arrow pressed");
-            //Instruction
             min = guess;
-            guess = (max + min) / 2;
-            print("Higher or lower than " + guess);
-            //Can you find and Fix the 999 bug?
+            NextGuess();
         }
-
+       
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            //print("Down arrow pressed");
-            //Challenge
             max = guess;
-            guess = (max + min) / 2;
-            print("Higher or lower than " + guess);
+            NextGuess();
         }
 
         else if(Input.GetKeyDown(KeyCode.Return))
         {
                 print("I won!");
         }    
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        print("Higher or lower than " + guess);
     }
 }
