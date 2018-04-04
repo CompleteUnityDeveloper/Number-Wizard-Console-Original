98a9b5f985cedada308cc6fc945268f32d99cb3c
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-04 11:51:42 +0100

    Commenting Your Code

--- Number Wizard Remastered/Assets/NumberWizard.cs
+++ Number Wizard Remastered/Assets/NumberWizard.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization
+	// Use this for initialization THIS IS A COMMENT
 	void Start ()
     {
 		
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame THiS IS ALSO A COMMENT
 	void Update ()
     {
 		
 	}
 }
+*/
