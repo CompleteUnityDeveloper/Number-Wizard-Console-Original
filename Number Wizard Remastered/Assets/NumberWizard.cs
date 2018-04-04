594d6f65c9d3f090cba399549fd5b23239916811
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-04 14:15:19 +0100

    Introducing Variables
    Data Type: Interger
    Declaring Vs Instantiating

--- Number Wizard Remastered/Assets/NumberWizard.cs
+++ Number Wizard Remastered/Assets/NumberWizard.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization THIS IS A COMMENT
+	// Use this for initialization
 	void Start ()
     {
         print("Welcome to Number Wizard");
         print("Pick a number, but don't tell me!");
+
+        // 
+        int wholeNumber; // This is a declaration: A Variable of type interger, named wholeNumber 
+        int min = 1; // This declaration has been instantiated: An interger variable, named minimum, set to a value of 1.
+        int max = 1000; //Challenge: to declare and instantiate maximum variable
+
+        //Check the console for warnings- we have declared and instantiated but have'nt used any of these variables!
+
     }
 	
-	// Update is called once per frame THiS IS ALSO A COMMENT
+	// Update is called once per frame
 	void Update ()
     {
 		
