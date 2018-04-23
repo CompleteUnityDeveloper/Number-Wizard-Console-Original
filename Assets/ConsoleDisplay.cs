72477fb5f1846bedc04a29faeee0b1252626690b
Ben Tristem <github@bentristem.com>
2018-04-23 21:57:58 +0100

    Using objects

---
+++ Assets/ConsoleDisplay.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleDisplay : MonoBehaviour
+{
+	// configuration parameters
+    [SerializeField] int lowestAllowed = 1;
+    [SerializeField] int highestAllowed = 1000;
+
+    // state variables
+
+    // references to other objects
+    NumberWizard numberWizard;
+
+    void Start()
+    {
+        Debug.Assert(highestAllowed > lowestAllowed); // simple check
+        numberWizard = new NumberWizard(lowestAllowed, highestAllowed);
+        DisplayIntro();
+    }
+
+    void Update()
+    {
+        RespondToKeys();
+    }
+
+    private void DisplayIntro()
+    {
+        Debug.Log("========================");
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a number in your head, but don't tell me!");
+
+        Debug.Log("Pick a number between " + lowestAllowed + " and " + highestAllowed);
+
+        var initialGuess = numberWizard.GetGuess();
+        Debug.Log("Is the number higher or lower than " + initialGuess + "?");
+        Debug.Log("Up = higher, down = lower, return = equal");
+    }
+
+    private void RespondToKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            numberWizard.GetHigherGuess();
+            var newGuess = numberWizard.GetGuess();
+            Debug.Log("Higher or lower than " + newGuess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            numberWizard.GetLowerGuess();
+            var newGuess = numberWizard.GetGuess();
+            Debug.Log("Higher or lower than " + newGuess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I guessed your number!");
+            Start();
+        }
+    }
+}
--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,80 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class NumberWizard : MonoBehaviour
+public class NumberWizard
 {
-    // configuration parameters
-    [SerializeField] int lowestAllowed = 1;
-    [SerializeField] int highestAllowed = 1000;
-
     // state variables
-    int min;
-    int max;
-    int guess;
-    int move;
-	
-	void Start ()
-    {
-        Debug.Assert(highestAllowed > lowestAllowed); // simple check
-        StartGame();
-	}
-	
-    // Update is called once per frame
-    void Update()
-    {
-        RespondToKeys();
-    }
+    private int min;
+    private int max;
+    private int guess;
 
-	private void StartGame ()
-    {
-        ResetStateVariables();
-        DisplayIntro(); // not order sensitive because of guess
-    }
-
-    private void ResetStateVariables()
+    public NumberWizard(int lowestAllowed, int highestAllowed)
     {
         min = lowestAllowed;
-        max = highestAllowed + 1; // because Unity rounds-down
+        max = highestAllowed + 1; // because of rounding-down
         guess = max / 2;
-        move = 1;
     }
 
-    private void DisplayIntro()
+    public int GetGuess()
     {
-        Debug.Log("========================");
-        Debug.Log("Welcome to Number Wizard");
-        Debug.Log("Pick a number in your head, but don't tell me!");
-
-        Debug.Log("The highest number you can pick is " + max);
-        Debug.Log("The lowest number you can pick is " + min);
-
-        Debug.Log("Is the number higher or lower than " + guess + "?");
-        Debug.Log("Up = higher, down = lower, return = equal");
+        return guess;
     }
 
-    private void RespondToKeys()
+    public int GetHigherGuess()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("I guessed your number in this many moves: " + move);
-            StartGame();
-        }
-    }
-
-    private void NextGuess ()
-    {
+        min = guess;
         guess = (max + min) / 2;
-        Debug.Log ("Higher or lower than " + guess);
-        move++;
-	}
+        return guess;
+    }
+
+    public int GetLowerGuess()
+    {
+        max = guess;
+        guess = (max + min) / 2;
+        return guess;
+    }
 }
